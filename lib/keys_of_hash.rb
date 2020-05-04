620c06d38f1d643cc1b84884f945f24f2b36a18c
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-04 16:25:04 -0400

    completed lab

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    ans = []
+    arguments.each do |arg|
+      self.each{ |key, value| ans << key if arg == value }
+    end
+    ans.flatten
   end
 end
